ec2fa565382bf7fc59fbd7c064df3649dab2c93b
Istorn <istorn.one@gmail.com>
2018-02-14 14:59:00 +0100

    test
    test

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-var express= require('express');
-var app=express();
-var mongoose=require('mongoose');
-var config=require('./config');
-var apiMain=require('./controllers/apiMain');
-
-var port=process.env.PORT || 3000;
-
-app.use('/', express.static(__dirname + '/public'));
-
-app.set('view engine', 'ejs');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
+var apiMain = require('./controllers/apiMain');
 
 mongoose.connect(config.getDbConnString());
-
+var port = process.env.PORT || 3000;
 apiMain(app);
 
-app.listen(port, ()=>{
-  console.log("in ascolto sulla porta "+port);
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.render('index', { name: 'Emanuele' });
+});
+
+app.listen(port, () => {
+  console.log('in ascolto sulla porta ' + port);
 });
--- models/membroModel.js
+++ models/membroModel.js
@@ -1,13 +1,13 @@
-var mongoose=require('mongoose');
-var Schema=mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var membroSchema=new Schema({
-    nome: String,
-    cognome: String,
-    datanascita: Date,
-    email: String,
-    telefono: String
+var membroSchema = new Schema({
+  nome: String,
+  cognome: String,
+  datanascita: Date,
+  email: String,
+  telefono: String
 });
 
-var Membri=mongoose.model('Membri',membroSchema);
-module.exports=Membri;
+var Membri = mongoose.model('Membri', membroSchema);
+module.exports = Membri;
